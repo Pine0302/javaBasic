48223c9ce24d2315e5dd478e8f4fc747ab6cb9a9
shenchaosong <shenchaosong@yidaoit.cn>
2020-09-15 17:35:16 +0800

    init

--- info/HelloWorld.java
+++ info/HelloWorld.java
@@ -2,7 +2,7 @@ import jdk.dynalink.beans.StaticClass;
 
 public class HelloWorld{
     public static void main(String[] args){
-        System.out.println("你好，世界！");
+        System.out.println("\"你好，世界！\"");
         int c = add(3,4);
         System.out.println(c);
     }
