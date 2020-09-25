464c63b1ead3ab68a2d5ebb5271ce0f31b8dbf67
shenchaosong <shenchaosong@yidaoit.cn>
2020-09-25 08:26:17 +0800

    class-duotai

--- class/DuoTaiDemo.java
+++ class/DuoTaiDemo.java
@@ -14,6 +14,44 @@ import org.w3c.dom.ls.LSException;
  *    1.必须有关系：继承/实现
  *    2.通常都有重写操作。  
  * 调父类的方法，运行子类的内容
+ * [子类的特有方法如何调用]
+    Animal a = new Dog();//Animal 是父类型, new Dog() 是子对象,
+    但是父类型引用指向子类对象时,这就是让子类对象进行了类型的提升(向上转型),
+    向上转型好处:提高了扩展性,隐藏了子类型.弊端:不能使用子类型方法
+    如果想要使用子类型的特有方法,只有子类型可以用.
+    可以向下转型,强制转换.
+    Animal a = new Dog();
+    (Dog) d = (Dog) a; //将a转型为Dog类型
+    d.lookHome();
+    向下转型什么时候用?当需要使用子类型的特有内容时.
+
+    注意:无论向上还是向下转型,最终都是子类对象做着类型的变化.
+
+    [向下转型的注意事项]
+    Animal a = new Dog();
+    Cat c = (Cat) a;  //向下转型 因为不明确具体子类对象类型,所以容易引发ClassCastException异常.
+    所以为了避免这个问题,需要在向下转型前,做类型判断.
+    判断类型用的是关键字 instanceof
+    比如:
+    if(a instanceof Cat)  //a指向的类型是Cat类型
+    {
+        Cat c = (Cat) a;
+        c.catchMouse();
+    }
+    elseif(a instanceof Dog)
+    {
+        Dog d = (Dog) a;
+        d.lookHome();
+    }
+
+ * [转型总结]
+ 1.什么时候使用向上转型呢?
+    提高程序的扩展性,不关系子类型(子类型被隐藏)
+    (需要用子类特有的方法吗?不需要,那就向上转型)
+ 2.什么时候使用向下转型呢?
+    需要使用子类型的特有方法时.
+    但是一定要使用 instanceof方法 进行类型判断,避免发生ClassCastException异常.
+
  */
 
 class DuotaiDemo  {
---
+++ class/DuoTaiTest.java
@@ -0,0 +1,83 @@
+import org.w3c.dom.ls.LSException;
+
+/**
+ * 笔记本电脑 鼠标 键盘 USB
+ * 笔记本电脑使用USB接口 兼容各种外设 
+ * 鼠标键盘等外设实现USB接口 
+ */
+
+class DuotaiTest  {
+    public static void main(String[] args) {
+        Mouse mouse = new Mouse();
+        Keybord keybord = new Keybord();
+        NoteBook noteBookA = new NoteBook();
+        noteBookA.useUsb(null);
+        noteBookA.useUsb(mouse);
+        noteBookA.useUsb(keybord);
+    }
+}
+
+interface USB{
+    public void open();
+    public void run();
+    public void close();
+}
+class NoteBook 
+{
+    public void run()
+    {
+        System.out.println("noteook run");
+    }
+
+    public void useUsb(USB usb)
+    {
+        if(usb!=null){
+            usb.open();
+            usb.run();
+            usb.close();
+        }else{
+            System.out.println("no usb device dected");
+        }
+        
+    }
+}
+
+class Mouse implements USB
+{
+    public void open()
+    {
+        System.out.println("mouse open");
+    }
+
+    public void run()
+    {
+        System.out.println("mouse run");
+    }
+
+    public void close()
+    {
+        System.out.println("mouse close");
+    }
+}
+
+
+
+class Keybord implements USB
+{
+    public void open()
+    {
+        System.out.println("keybord open");
+    }
+
+    public void run()
+    {
+        System.out.println("keybord run");
+    }
+
+    public void close()
+    {
+        System.out.println("keybord close");
+    }
+}
+
+
